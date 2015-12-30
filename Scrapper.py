8758804ceaea56c3ae34062137591e644a0ed13d
root <gomescleve@gmail.com>
2015-12-30 09:53:09 +0000

    tts bash file added

--- Scrapper.py
+++ Scrapper.py
@@ -3,6 +3,7 @@ import json
 import datetime
 import os
 from pygame import mixer
+from espeak import espeak
 
 r = requests.get('http://52.25.60.112/localsensors/rest.php')
 jdata = r.json();
@@ -17,13 +18,15 @@ difference =  systemdTime- serverdTime ;
 cutoffTime = 30*60;
 
 if difference.total_seconds() <= cutoffTime:
-	print(systemdTime)
-	print(serverdTime)
-	print(difference.total_seconds())
-	mixer.init()
-	mixer.music.load('/var/pythonFiles/stormyPython/1.mp3');
-	mixer.music.play();
-	while mixer.music.get_busy() == True:
-		continue
+        os.system("/var/pythonFiles/stormyPython/tts.sh This is it > file.log 2>&1")
+	#espeak.synth("HelloInstructables!")
+	#print(systemdTime)
+	#print(serverdTime)
+	#print(difference.total_seconds())
+	#mixer.init()
+	#mixer.music.load('/var/pythonFiles/stormyPython/1.mp3');
+	#mixer.music.play();
+	#while mixer.music.get_busy() == True:
+	#	continue
 
 
---
+++ command.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+cd /var/pythonFiles/stormyPython/
+python Scrapper.py 
