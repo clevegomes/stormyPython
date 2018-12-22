c53e42ea6855292a383607d8751d5cac94c53082
gomescleve <gomescleve@gmail.com>
2018-12-22 21:33:10 +0400

    added tracks

--- crib.py
+++ crib.py
@@ -1,16 +1,4 @@
-import requests
-
-import json
-import datetime
-import os
 from pygame import mixer
-import urllib2
-from urllib2 import urlopen
-import re
-import cookielib
-from cookielib import CookieJar
-import time
-import sys
 
 
 
@@ -22,19 +10,19 @@ while mixer.music.get_busy():
     time.sleep(1)
 
 mixer.music.load('silent-night.mp3')
-mixer.music.set_volume(0.8)
+mixer.music.set_volume(0.5)
 mixer.music.play(loops=0, start=0.0)
 while mixer.music.get_busy():
     time.sleep(1)
 
 mixer.music.load('noel.mp3')
-mixer.music.set_volume(0.8)
+mixer.music.set_volume(0.5)
 mixer.music.play(loops=0, start=0.0)
 while mixer.music.get_busy():
     time.sleep(1)
 
 mixer.music.load('we-three-kings.mp3')
-mixer.music.set_volume(0.8)
+mixer.music.set_volume(0.5)
 mixer.music.play(loops=0, start=0.0)
 while mixer.music.get_busy():
     time.sleep(1)
