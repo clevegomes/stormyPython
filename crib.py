bf9b3cc86f8cd740dbb94a35f8ef093b79b29081
gomescleve <gomescleve@gmail.com>
2018-12-22 22:00:22 +0400

    added tracks

--- crib.py
+++ crib.py
@@ -5,25 +5,25 @@ import time
 
 
 mixer.init()
-mixer.music.load('farm-animals.mp3')
+mixer.music.load('~/projects/stormyPython/crib.pyfarm-animals.mp3')
 mixer.music.set_volume(0.2)
 mixer.music.play(loops=0, start=0.0)
 while mixer.music.get_busy():
     time.sleep(1)
 
-mixer.music.load('silent-night.mp3')
+mixer.music.load('~/projects/stormyPython/crib.pysilent-night.mp3')
 mixer.music.set_volume(0.5)
 mixer.music.play(loops=0, start=0.0)
 while mixer.music.get_busy():
     time.sleep(1)
 
-mixer.music.load('noel.mp3')
+mixer.music.load('~/projects/stormyPython/crib.pynoel.mp3')
 mixer.music.set_volume(0.5)
 mixer.music.play(loops=0, start=0.0)
 while mixer.music.get_busy():
     time.sleep(1)
 
-mixer.music.load('we-three-kings.mp3')
+mixer.music.load('~/projects/stormyPython/crib.pywe-three-kings.mp3')
 mixer.music.set_volume(0.5)
 mixer.music.play(loops=0, start=0.0)
 while mixer.music.get_busy():
