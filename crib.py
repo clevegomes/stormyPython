4e1746510093a656f0b71532d5f08e38b5110987
gomescleve <gomescleve@gmail.com>
2018-12-22 20:53:51 +0400

    quick fix

--- crib.py
+++ crib.py
@@ -16,7 +16,8 @@ import sys
 
 mixer.init()
 mixer.music.load('farm-animals.mp3')
-mixer.music.set_volume(1)
+mixer.music.set_volume(0.7)
 mixer.music.play(loops=0, start=0.0)
+while pygame.mixer.music.get_busy():
 # mixer.music.fadeout(9000)
-time.sleep(9)
+# time.sleep(9)
