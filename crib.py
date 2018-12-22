ffaab9676523acb7b65e6c80b34b52d3a14f2d4b
gomescleve <gomescleve@gmail.com>
2018-12-22 21:16:26 +0400

    quick fix

--- crib.py
+++ crib.py
@@ -16,7 +16,13 @@ import sys
 
 mixer.init()
 mixer.music.load('farm-animals.mp3')
-mixer.music.set_volume(0.5)
+mixer.music.set_volume(0.3)
+mixer.music.play(loops=0, start=0.0)
+while mixer.music.get_busy():
+    time.sleep(1)
+
+mixer.music.load('we-three-kings.mp3')
+mixer.music.set_volume(0.8)
 mixer.music.play(loops=0, start=0.0)
 while mixer.music.get_busy():
     time.sleep(1)
