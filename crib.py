66676404a9dcb154e412dfb488d8839dc4dc8807
gomescleve <gomescleve@gmail.com>
2018-12-22 21:26:58 +0400

    added tracks

--- crib.py
+++ crib.py
@@ -21,6 +21,18 @@ mixer.music.play(loops=0, start=0.0)
 while mixer.music.get_busy():
     time.sleep(1)
 
+mixer.music.load('silent-night.mp3')
+mixer.music.set_volume(0.8)
+mixer.music.play(loops=0, start=0.0)
+while mixer.music.get_busy():
+    time.sleep(1)
+
+mixer.music.load('noel.mp3')
+mixer.music.set_volume(0.8)
+mixer.music.play(loops=0, start=0.0)
+while mixer.music.get_busy():
+    time.sleep(1)
+
 mixer.music.load('we-three-kings.mp3')
 mixer.music.set_volume(0.8)
 mixer.music.play(loops=0, start=0.0)
