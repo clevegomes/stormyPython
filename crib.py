4459352034246e7dbbc42dc92226c3c48c6b09d7
gomescleve <gomescleve@gmail.com>
2018-12-23 23:10:19 +0400

    blick speed

--- crib.py
+++ crib.py
@@ -50,7 +50,7 @@ def blinkRelay(pin, SleepTimeL = 2, count = 2):
         GPIO.cleanup()
 
 
-blinkRelay(1)
+blinkRelay(2,1)
 playMusic('/home/pi/projects/stormyPython/farm-animals.mp3')
 playMusic('/home/pi/projects/stormyPython/silent-night.mp3')
 playMusic('/home/pi/projects/stormyPython/noel.mp3')
